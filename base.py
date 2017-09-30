51dc762dcf1076e0c6f93e6221c76b39d69e0629
wuzhuquan <wu_zhuquan@qq.com>
2017-09-30 18:26:32 +0800

    20170929

--- base.py
+++ base.py
@@ -1,24 +1,28 @@
 import os
 import unittest
+import log_config
 from appium import webdriver
 from time import sleep
-import get_driver,get_element
-
+import get_driver
+from get_element import Element
 # Returns abs path relative to this file and not cwd
 PATH = lambda p: os.path.abspath(
     os.path.join(os.path.dirname(__file__), p)
 )
 
 
-class ChatTest(unittest.TestCase):
+class ChatTest(unittest.TestCase,Element):
+    logging = log_config.getlogger("")
     def setUp(self):
         self.driver = get_driver.get_driver()
+
 
     def tearDown(self):
         self.driver.quit()
 
     def test_chat(self):
-        el = self.driver.find_element_by_id("digit_4")
+
+        el = self.get_id("main_too_keen")
         el.click()
 
 if __name__ == '__main__':
--- get_driver.py
+++ get_driver.py
@@ -1,16 +1,26 @@
 from appium import webdriver
+from selenium import webdriver
 # ecoding=utf-8
 __author__ = "Sven_Weng"
 
 
 def get_driver():
-
+    chrome_option = webdriver.ChromeOptions()
+    chrome_option.add_experimental_option("androidProcess", "com.tencent.mm:tools")
     desired_caps = {'platformName': 'Android',
-                    'platformVersion': '7.0',
+                    'platformVersion': '7.1.1',
                     'deviceName': 'CB512BPKD9',
-                    'appPackage': 'com.android.calculator2',
-                    'appActivity': '.Calculator'
+                    #微信
+                    'appPackage': 'com.tencent.mm',
+                    'appActivity': 'ui.LauncherUI',
+                    'ChromeOptions.CAPABILITY':'chrome_option',
+                    #技师APP
+                    #'appPackage': 'com.xmd.technician',
+                    #'appActivity': '.window.WelcomeActivity',
+                    #'appWaitActivity': '.window.MainActivity'
+
                     }
+
     driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
     driver.implicitly_wait(30)
     return driver
